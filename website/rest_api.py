92f2e1085ac09c563a9abfd97b1ef0e41c4e1d7f
otterHooligan <kristy.majetich@yahoo.com>
2021-11-03 15:49:56 -0400

    Added more rest_api endpoints

--- website/rest_api.py
+++ website/rest_api.py
@@ -1,8 +1,7 @@
 from flask_restful import Resource, fields, marshal_with
-from .models import PlateAppearance
+from .models import PlateAppearance, Player, GameData
 
-
-resource_fields = {
+pa_resource_fields = {
     'paID': fields.Integer,
     'league': fields.String,
     'season': fields.Integer,
@@ -37,22 +36,74 @@ resource_fields = {
     'pr1B': fields.Integer,
     'prAB': fields.Integer
 }
+player_resource_fields = {
+    'playerID': fields.Integer,
+    'playerName': fields.String,
+    'Team': fields.String,
+    'batType': fields.String,
+    'pitchType': fields.String,
+    'pitchBonus': fields.String,
+    'hand': fields.String,
+    'priPos': fields.String,
+    'secPos': fields.String,
+    'tertPos': fields.String,
+    'redditName': fields.String,
+    'discordName': fields.String,
+    'discordID': fields.Integer,
+    'status': fields.Integer,
+    'posValue': fields.Integer
+}
 
 
 class PitcherData(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(pa_resource_fields)
     def get(self, league, player_id):
         pitch_data = PlateAppearance.query.filter_by(pitcherID=player_id, league=league).all()
         return pitch_data
 
 
 class HitterData(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(pa_resource_fields)
     def get(self, league, player_id):
         pitch_data = PlateAppearance.query.filter_by(hitterID=player_id, league=league).all()
         return pitch_data
 
 
+class AllPlateAppearances(Resource):
+    @marshal_with(pa_resource_fields)
+    def get(self):
+        return PlateAppearance.query.all()
+
+
+class PlateAppearnacesByLeague(Resource):
+    @marshal_with(pa_resource_fields)
+    def get(self, league):
+        return PlateAppearance.query.filter_by(league=league).all()
+
+
+class AllPlayers(Resource):
+    @marshal_with(player_resource_fields)
+    def get(self):
+        return Player.query.all()
+
+
+class PlayerID(Resource):
+    @marshal_with(player_resource_fields)
+    def get(self, playerID):
+        return Player.query.filter_by(playerID=playerID).first()
+
+
+class PlayerName(Resource):
+    @marshal_with(player_resource_fields)
+    def get(self, player_name):
+        return Player.query.filter_by(playerName=player_name).first()
+
+
 def add_resources(api):
-    api.add_resource(PitcherData, '/plateappearances/pitching/<string:league>/<int:player_id>')
-    api.add_resource(HitterData, '/plateappearances/batting/<string:league>/<int:player_id>')
+    api.add_resource(PitcherData, '/api/plateappearances/pitching/<string:league>/<int:player_id>')
+    api.add_resource(HitterData, '/api/plateappearances/batting/<string:league>/<int:player_id>')
+    api.add_resource(AllPlateAppearances, '/api/plateappearances')
+    api.add_resource(PlateAppearnacesByLeague, '/api/plateappearances/<string:league>')
+    api.add_resource(AllPlayers, '/api/players')
+    api.add_resource(PlayerID, '/api/players/<int:player_id>')
+    api.add_resource(PlayerName, '/api/players/<string:player_name>')
